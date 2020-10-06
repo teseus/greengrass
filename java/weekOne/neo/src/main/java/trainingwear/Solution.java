6f164fbc3e5fab7ceca0bb19c3f5ba92d3af5cab
Neo <teseus@wemakeprice.com>
2020-10-07 08:50:36 +0900

    First Week. 프로그래머스 체육복.

---
+++ java/weekOne/neo/src/main/java/trainingwear/Solution.java
@@ -0,0 +1,8 @@
+package trainingwear;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ java/weekOne/readme.md
@@ -0,0 +1,5 @@
+# 첫번째 문제
+greedy 알고리즘의 난이도 1레벨의 문제입니다. 
+아래의 url 에 있는 문제를 푸시면 됩니다. 홧팅.
+
+https://programmers.co.kr/learn/courses/30/lessons/42862
