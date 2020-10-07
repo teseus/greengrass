8912cbbc968948d2b0329b2d0f176b1fd061e8b3
Neo <teseus@wemakeprice.com>
2020-10-07 23:32:43 +0900

    First Week. 프로그래머스 체육복. neo 2차시도. 먼저 자기것 부터 채워야 함.

--- java/weekOne/neo/src/main/java/trainingwear/Solution.java
+++ java/weekOne/neo/src/main/java/trainingwear/Solution.java
@@ -9,22 +9,27 @@ class Solution {
             students[i] = 1;
         }
         for (int i : lost) {
-            int idx = i - 1;
-            students[idx] = 0;
+            students[i - 1] = 0;
         }
-        for (int i : reserve) {
-            int idx = i - 1;
-            if(students[idx] == 0)
-                students[idx] = 1;
-            else if (idx > 1 && students[idx-1]==0)
-                students[idx-1] = 1;
-            else if (idx < n-1 && students[idx+1]==0)
-                students[idx+1] = 1;
+
+        for (int i: reserve) {
+            students[i - 1] = ++students[i - 1];
         }
+
         int sum = 0;
-        for (int student : students) {
-            sum += student;
+        for (int i = 0; i < students.length; i++) {
+            if(students[i] == 0){
+                if(i > 0 && students[i-1] == 2) {
+                    students[i-1]--;
+                    students[i]++;
+                } else if(i < n-1 && students[i+1] == 2) {
+                    students[i+1]--;
+                    students[i]++;
+                }
+            }
+            sum += students[i]>0?1:0;
         }
+
         return sum;
     }
 
@@ -35,7 +40,11 @@ class Solution {
         assertEquals(5, new Solution().solution(5, new int[]{2, 4}, new int[] {2,5}));
         assertEquals(6, new Solution().solution(7, new int[]{2, 4}, new int[] {2, 7}));
         assertEquals(4, new Solution().solution(5, new int[]{1, 2, 3}, new int[] {2, 3, 4}));
+        assertEquals(4, new Solution().solution(5, new int[]{2, 3, 4}, new int[] {1, 2, 3}));
         assertEquals(5, new Solution().solution(5, new int[]{1, 2, 3}, new int[] {1, 2, 4}));
+        assertEquals(2, new Solution().solution(3, new int[]{3}, new int[] {1}));
         assertEquals(1, new Solution().solution(1, new int[]{1}, new int[] {1}));
+        assertEquals(3, new Solution().solution(3, new int[]{2}, new int[] {1,3}));
+        assertEquals(4, new Solution().solution(5, new int[]{1}, new int[] {4,5}));
     }
 }
